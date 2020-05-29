d380dc910bec9fdd2f1cd5494cb0f22a13828c51
marcusbai <378978524@qq.com>
2020-05-29 19:43:49 +0800

    更新

--- MainApp.py
+++ MainApp.py
@@ -1,22 +1,18 @@
-#from PySide2.QtWidgets import QApplication, QMessageBox
-#from PySide2.QtUiTools import QUiLoader
-from PyQt5 import QtWidgets, ui
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.uic import loadUi
 
 import sys
 class MainApp:
 
     def __init__(self):
-        # 从文件中加载UI定义
-        # 从 UI 定义中动态 创建一个相应的窗口对象
-        # 注意：里面的控件对象也成为窗口对象的属性了
-        # 比如 self.ui.button , self.ui.textEdit
-        self.ui = QUiLoader().load('MainApp.ui')
+        
+        self.ui = loadUi('MainApp.ui')
 
         #self.ui.button.clicked.connect(self.handleCalc)
 
     #def handleCalc(self):
 
 app = QApplication([])
-ma = MainApp()
-ma.ui.show()
+MainApp = MainApp()
+MainApp.ui.show()
 app.exec_()
